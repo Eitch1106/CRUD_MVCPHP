77ac5cb8aa49a59f199420756250359575bed63e
eitch <garcia.irv@gmail.com>
2022-02-18 12:42:26 -0600

    SUBIDA DATABLES EN PUBLIC Y ACTUALIZACION DEL CURSO 18/02/2022

--- view/MntProducto/index.php
+++ view/MntProducto/index.php
@@ -714,7 +714,7 @@
     </div><!-- br-mainpanel -->
     <!-- ########## END: MAIN PANEL ########## -->
 
-    <?php require_once("modalmantenimiento.php");?>
+     <?php require_once("modalmantenimiento.php");?> 
 
     <script src="../../public/lib/jquery/jquery.js"></script>
     <script src="../../public/lib/popper.js/popper.js"></script>
